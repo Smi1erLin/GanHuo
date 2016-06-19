4012de1fa5f6994cce11226c83083f5f28bba11d
Smi1erLin <2011907149@qq.com>
2016-06-20 05:46:27 +0800

    Update: RxJava Timer操作符

--- GanHuo/src/main/java/echohce/cn/gank/view/activity/SplashActivity.java
+++ GanHuo/src/main/java/echohce/cn/gank/view/activity/SplashActivity.java
@@ -2,15 +2,18 @@ package echohce.cn.gank.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.WindowManager;
 
+import java.util.concurrent.TimeUnit;
+
 import echohce.cn.gank.R;
+import rx.Observable;
+import rx.functions.Action1;
 
 public class SplashActivity extends AppCompatActivity {
-    Handler handler = new Handler();
+    //Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +25,25 @@ public class SplashActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_splash);
 
-        // 定时跳转
-        handler.postDelayed(new Runnable() {
+        // 定时跳转用到了timer操作符
+        Observable.timer(2, TimeUnit.SECONDS).subscribe(new Action1<Long>() {
             @Override
-            public void run() {
+            public void call(Long aLong) {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                // Activity切换效果，必须放在startActivity之后。  -- Android开发艺术探索
                 overridePendingTransition(0, 0);
                 SplashActivity.this.finish();
             }
-        }, 1500); // 1500ms后进行跳转
+        });
+        // 定时跳转
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+//                // Activity切换效果，必须放在startActivity之后。  -- Android开发艺术探索
+//                overridePendingTransition(0, 0);
+//                SplashActivity.this.finish();
+//            }
+//        }, 1500); // 1500ms后进行跳转
     }
 
     @Override
